92c6f2679a0fef2b13ac86c3a325942d6fecdc82
Leo Nuneza <leiyu876@yahoo.com>
2022-05-06 11:23:26 +0800

    Add code from source page mupati: see readme file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+window.Vue = Vue;
+
+Vue.component("broadcaster", require("./components/Broadcaster.vue").default);
+Vue.component("viewer", require("./components/Viewer.vue").default);
+
+const app = new Vue({
+    el: '#app'
+});
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/streaming', 'App\Http\Controllers\WebrtcStreamingController@index');
+Route::get('/streaming/{streamId}', 'App\Http\Controllers\WebrtcStreamingController@consumer');
+Route::post('/stream-offer', 'App\Http\Controllers\WebrtcStreamingController@makeStreamOffer');
+Route::post('/stream-answer', 'App\Http\Controllers\WebrtcStreamingController@makeStreamAnswer');
